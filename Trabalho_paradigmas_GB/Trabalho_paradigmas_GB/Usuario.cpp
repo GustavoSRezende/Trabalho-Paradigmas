883d438416684329da619592fcfe7d1d4517fcd0
Rezendegu1 <gustavosbardelottorezende@gmail.com>
2026-04-17 01:41:14 -0300

    Versao Final

--- Trabalho_paradigmas_GB/Trabalho_paradigmas_GB/Usuario.cpp
+++
@@ -1,9 +0,0 @@
-#include "Usuario.h"
-using namespace std;
-
-// construtor basico, so passa pra cima nas subclasses
-Usuario::Usuario(const string& nome, int id)
-    : nome(nome), id(id) {}
-
-string Usuario::getNome() const { return nome; }
-int         Usuario::getId()   const { return id; }
